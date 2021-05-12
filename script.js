b4aa8485706495da1c12b9a7f1d16ae5de437959
Alex93pa <Ale.pug2@gmail.com>
2021-05-12 12:53:57 +0200

    album

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 new  Vue ({
     el: "#app",
     data: {
-        filteredData = []
+        filteredData : [],
+        albumsList : []
     },
     methods: {
+        getGenereList() {
+            const finalList = []
+
+            albumsList.forEach((element) => {
+                if(!finalList.includes(element.genre)) {
+                    finalList.push(element.genre)
+                }
+            })
+
+        },
         onSelectChange(event) {
             const select = event.currentTarget
             select.value
@@ -14,9 +25,11 @@ new  Vue ({
     mounted() {
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
-            const albumsList = resp.data.response;
+            this.albumsList = resp.data.response;
+            // const albumsList = resp.data.response;
+            // this.albumArrays.push(resp.data.response)
+            // this.filteredData.push(resp.data.response.genere)
 
-            
         })
     },
    
