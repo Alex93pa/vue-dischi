598cb51450a5cc76c6772c9f854b94f657a056c6
Alex93pa <Ale.pug2@gmail.com>
2021-05-13 13:08:35 +0200

    fine

--- script.js
+++ script.js
@@ -2,45 +2,59 @@ new  Vue ({
     el: "#app",
     data: {
         filteredData : [],
-        albumsList : []
+        albumsList : [],
+        genreList : [],
+        genreToFilter : ""
     },
     methods: {
-        getGenereList() {
-            const genreList = []
+        onSelectChange() {
+            if(this.genreToFilter === "") {
+                this.filteredData = [...this.albumsList]                
+                return
+            }
 
-            this.albumsList.forEach((element) => {
-                if(!genreList.includes(element.genre)) {
-                    genreList.push(element.genre)
+            const newFilteredData = this.albumsList.filter((album) => {
+                return album.genre === this.genreToFilter
+            })
+
+            this.filteredData = newFilteredData
+
+        },
+        createGenresOptions() {
+
+            this.albumsList.forEach((album) => {
+                if(!this.genreList.includes(album.genre)) {
+                    this.genreList.push(album.genre);
+
                 }
             })
-            return genreList
         },
-        // selectGenre(event) {
-        //     const select = event.currentTarget
-        //     select.value
-
-        //     // this.filteredData = 
-        // },
-        orderDate: function() {
-            this.items.sort((a, b) => {
-                if (Date.parse(a.year) > Date.parse(b.year)) {
-                   return 1
-                } else if (Date.parse(a.year) < Date.parse(b.year)) {
-                   return -1
-                } else {
-                   return 0
-                }
-              })
-            // return this.item.sort((a, b) => new Date(a.year) - new Date(b.year) );
-        }
+        // orderDate() {
+        //     this.items.sort((a, b) => {
+        //         if (Date.parse(a.year) > Date.parse(b.year)) {
+        //            return 1
+        //         } else if (Date.parse(a.year) < Date.parse(b.year)) {
+        //            return -1
+        //         } else {
+        //            return 0
+        //         }
+        //       })
+        //     // return this.item.sort((a, b) => new Date(a.year) - new Date(b.year) );
+        // }
     },
     mounted() {
+        
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
             this.albumsList = resp.data.response;
 
+            // this.albumsList.push(...incomingAlbumsList);
+            // this.filteredData.push(...incomingAlbumsList);
 
+
+            this.createGenresOptions();
         })
+
     },
    
 })
